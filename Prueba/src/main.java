0b9f6f6c9423fc891a481701069ac7eb720e98f3
Daniel <danieldrq89@educastur.es>
2024-09-30 12:11:35 +0200

    Ejercicios Programacion

--- Prueba/src/main.java
+++ Prueba/src/main.java
@@ -3,7 +3,44 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//EJERCICIO 1
+		int a = -9;
+		int b = 21;
+		int c = -35;
+		int d = 51;
+		int e = 33;
+		int f = (a + b) * e;
+		
+		System.out.println("Suma de a + b * e : " + (f));
+		System.out.println("Division d / c : " + (d / c));
+		
+		System.out.println("-----------------------------------------");
+		// EJERCICIO 2
+		System.out.println(a < b || c != d);
+			
+		System.out.println("-----------------------------------------");
+		//EJERCICIO 3
+		int year = 1988;
+		int mod = year % 4;
+		
+		System.out.println(mod == 0);
+		System.out.println("-----------------------------------------");
 
+		//EJERCICIO 4
+		
+		if(mod == 0) {
+			System.out.println("Año bisiesto");
+			}
+		System.out.println("-----------------------------------------");
+		//EJERCICIO 5
+		int x = 1;
+		int y = 1;
+		int z = 2;
+		double elevado = Math.pow(x, y); 
+		int convertido = (int)elevado ; // De double a int
+		
+		System.out.println(Math.multiplyExact(z, convertido));
 	}
-
 }
+
